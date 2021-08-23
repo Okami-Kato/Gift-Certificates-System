33ea53ffb47f9f231d86c93ff9585343affc5fcc
Okami-Kato <barkan.makar@gmail.com>
2021-08-23 14:05:32 +0300

    equals, hashCode, toString

--- entity/src/main/java/com/epam/esm/entity/Tag.java
+++ entity/src/main/java/com/epam/esm/entity/Tag.java
@@ -1,5 +1,7 @@
 package com.epam.esm.entity;
 
+import java.util.Objects;
+
 public class Tag extends Entity{
     private String name;
 
@@ -13,5 +15,26 @@ public class Tag extends Entity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return name.equals(tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
