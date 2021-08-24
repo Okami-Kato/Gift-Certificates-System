d409db5fcce883d07c24424b7b1bf935e9807a06
Okami-Kato <barkan.makar@gmail.com>
2021-08-24 14:14:28 +0300

    updated tests

--- dao/src/test/java/com/epam/esm/dao/CertificateDaoImplTest.java
+++ dao/src/test/java/com/epam/esm/dao/CertificateDaoImplTest.java
@@ -5,6 +5,7 @@ import com.epam.esm.entity.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,9 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -61,7 +64,8 @@ class certificateDaoTest {
         firstCertificate = certificateDao.create(firstCertificate);
         int newAmount = certificateDao.getAll().size();
         assertEquals(oldAmount + 1, newAmount);
-        certificateDao.delete(firstCertificate.getId());
+        assertTrue(certificateDao.delete(firstCertificate.getId()));
+        assertFalse(certificateDao.delete(firstCertificate.getId()));
         int afterDeleteAmount = certificateDao.getAll().size();
         assertEquals(afterDeleteAmount, oldAmount);
     }
@@ -73,7 +77,7 @@ class certificateDaoTest {
         Optional<Certificate> oldCertificate = certificateDao.get(firstCertificate.getId());
         assertTrue(oldCertificate.isPresent());
         firstCertificate.setName("altered name");
-        certificateDao.update(firstCertificate);
+        assertTrue(certificateDao.update(firstCertificate));
         Optional<Certificate> updatedCertificate = certificateDao.get(firstCertificate.getId());
         assertTrue(updatedCertificate.isPresent());
         assertNotEquals(oldCertificate, updatedCertificate);
@@ -104,11 +108,13 @@ class certificateDaoTest {
         int oldSize = certificateDao.getAllByTags(firstTag.getId()).size();
         assertEquals(oldSize, 0);
         certificateDao.addTag(firstCertificate.getId(), firstTag.getId());
+        assertThrows(DataAccessException.class, () -> certificateDao.addTag(firstCertificate.getId(), firstTag.getId()));
         certificateDao.addTag(secondCertificate.getId(), firstTag.getId());
         certificateDao.addTag(thirdCertificate.getId(), secondTag.getId());
         int newSize = certificateDao.getAllByTags(firstTag.getId(), secondTag.getId()).size();
         assertEquals(newSize, 3);
         certificateDao.removeTag(secondCertificate.getId(), firstTag.getId());
+        assertFalse(certificateDao.removeTag(secondCertificate.getId(), firstTag.getId()));
         newSize = certificateDao.getAllByTags(firstTag.getId(), secondTag.getId()).size();
         assertEquals(newSize, 2);
     }
--- dao/src/test/java/com/epam/esm/dao/TagDaoImplTest.java
+++ dao/src/test/java/com/epam/esm/dao/TagDaoImplTest.java
@@ -1,47 +1,81 @@
 package com.epam.esm.dao;
 
+import com.epam.esm.entity.Certificate;
 import com.epam.esm.entity.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {SpringDaoTestConfig.class})
 class TagDaoImplTest {
-    Tag tag = new Tag("test");
+    Certificate certificate = Certificate.newBuilder()
+            .setName("name1")
+            .setDescription("duration")
+            .setDuration(10)
+            .setPrice(5)
+            .setCreateDate(LocalDate.now())
+            .setLastUpdateDate(LocalDate.now())
+            .build();
+    Tag firstTag = new Tag("test1");
+    Tag secondTag = new Tag("test2");
 
     @Autowired
-    TagDao tagDAO;
+    TagDao tagDao;
+
+    @Autowired
+    CertificateDao certificateDao;
 
     @Test
     @Transactional
     void delete() {
-        int oldAmount = tagDAO.getAll().size();
-        tag = tagDAO.create(tag);
-        int newAmount = tagDAO.getAll().size();
+        int oldAmount = tagDao.getAll().size();
+        firstTag = tagDao.create(firstTag);
+        int newAmount = tagDao.getAll().size();
         assertEquals(oldAmount + 1, newAmount);
-        tagDAO.delete(tag.getId());
-        int afterDeleteAmount = tagDAO.getAll().size();
+        assertTrue(tagDao.delete(firstTag.getId()));
+        assertFalse(tagDao.delete(firstTag.getId()));
+        int afterDeleteAmount = tagDao.getAll().size();
         assertEquals(afterDeleteAmount, oldAmount);
     }
 
     @Test
     @Transactional
     void create() {
-        int oldAmount = tagDAO.getAll().size();
-        tag = tagDAO.create(tag);
-        int newAmount = tagDAO.getAll().size();
+        int oldAmount = tagDao.getAll().size();
+        firstTag = tagDao.create(firstTag);
+        int newAmount = tagDao.getAll().size();
         assertEquals(oldAmount + 1, newAmount);
-        Optional<Tag> dbTag = tagDAO.get(tag.getId());
+        assertThrows(DataAccessException.class, () -> tagDao.create(firstTag));
+        Optional<Tag> dbTag = tagDao.get(firstTag.getId());
         assertTrue(dbTag.isPresent());
-        assertEquals(tag, dbTag.get());
+        assertEquals(firstTag, dbTag.get());
+    }
+
+    @Test
+    @Transactional
+    void getAllByCertificateId() {
+        firstTag = tagDao.create(firstTag);
+        secondTag = tagDao.create(secondTag);
+        certificate = certificateDao.create(certificate);
+        certificateDao.addTag(certificate.getId(), firstTag.getId());
+        certificateDao.addTag(certificate.getId(), secondTag.getId());
+        List<Tag> allByCertificateId = tagDao.getAllByCertificateId(certificate.getId());
+        assertEquals(allByCertificateId.size(), 2);
+        assertTrue(allByCertificateId.containsAll(Arrays.asList(firstTag, secondTag)));
     }
 }
